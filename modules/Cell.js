2899b846f257d94b1871dacb360d3eea3a5ad8ec
Leon Robert <ler@rmsiasi.onmicrosoft.com>
2020-01-29 16:27:55 +0200

    no ids for cells

--- modules/Cell.js
+++ modules/Cell.js
@@ -8,7 +8,6 @@ function Cell() {
 Object.assign(Cell.prototype, {
     createCell: function(){
         let cell = document.createElement("td");
-        cell.setAttribute("id", this.row + "_" + this.columns);
         cell.setAttribute("class", this.state);
         cell.onclick = () => this.cellClickHandler();
         this.domElement = cell;
